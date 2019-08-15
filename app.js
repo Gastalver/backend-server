4fbb7a63a2fc099d08862d585f6a37c74449b3db
Gastalver <mgastalver@gmail.com>
2019-08-15 14:29:12 +0200

    Añadido mongoose y la primera ruta

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-// Requires
+// Requires (Importación de librerías)
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
+
+// Conexión a la base de datos.
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',{ useNewUrlParser: true },(error,res) => {
+  if (error) throw error;
+    console.log('Mongo en puerto 27017: \x1b[32m%s\x1b[0m','Conectado con éxito');
+})
 
 // Rutas
 app.get(
